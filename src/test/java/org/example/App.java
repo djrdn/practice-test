9c338540681ee1e649b5f7eeb5866e009026b8a8
Dmytro <dimandinchik@gmail.com>
2025-04-26 13:35:33 +0300

    first autotest with testng

--- src/test/java/org/example/App.java
+++ src/test/java/org/example/App.java
@@ -1,30 +1,7 @@
 package org.example;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.concurrent.TimeUnit;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://rozetka.com.ua/");
-        driver.findElement(By.name("search")).sendKeys("Iphone 15");
-        driver.findElement(By.className("search-form__submit")).click();
-        try {
-            Thread.sleep(10000); // Задержка в 10 секунд
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        driver.quit();
-
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Запусти тесты через TestNG!");
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,38 +1,39 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import java.time.Duration;
+
+public class AppTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void searchIphoneOnRozetka() {
+        driver.get("https://rozetka.com.ua/");
+        driver.findElement(By.name("search")).sendKeys("Iphone 15");
+        driver.findElement(By.cssSelector("button.search-form__submit")).click();
+
+        // Здесь можно добавить ожидание элемента, чтобы удостовериться, что результаты появились
+        // Например:
+        // new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".goods-tile__title")));
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
